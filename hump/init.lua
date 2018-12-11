d3dc7aa856afdea25a0d226aad5bd0a3bf0a9772
Adam Vandolder <adam.vandolder@gmail.com>
2018-12-10 20:55:26 -0500

    Removed old HUMP library

--- hump/init.lua
+++
@@ -1,8 +0,0 @@
-return {
-  Camera = require('lib.hump.camera'),
-  Class = require('lib.hump.class'),
-  Gamestate = require('lib.hump.gamestate'),
-  Signal = require('lib.hump.signal'),
-  Timer = require('lib.hump.timer'),
-  Vector = require('lib.hump.vector'),
-}
